7317c279efba9b824f0987ae3b8596e0b74ddcf4
faysal725 <afaysal725@gmail.com>
2021-05-13 10:16:22 +0600

    final commit

---
+++ practice.js
@@ -0,0 +1,10 @@
+var number1 = "26";
+var number2 ="25";
+var process1 = "+";
+
+// console.log(typeof(process1));
+// console.log(typeof(process2));
+
+var total =eval (number1+ process1 + number2);
+
+console.log(total);
